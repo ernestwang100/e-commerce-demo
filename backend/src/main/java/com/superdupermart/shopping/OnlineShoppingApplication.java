3cdcfc738966f3459feb57da71c8b42f1ef5b754
Yu-En Wang <ernestwang100@gmail.com>
2026-01-09 07:11:16 -0500

    Configure Redis infrastructure and dependencies

--- backend/src/main/java/com/superdupermart/shopping/OnlineShoppingApplication.java
+++ backend/src/main/java/com/superdupermart/shopping/OnlineShoppingApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+import org.springframework.cache.annotation.EnableCaching;
+
 @SpringBootApplication
+@EnableCaching
 public class OnlineShoppingApplication {
 
 	public static void main(String[] args) {
